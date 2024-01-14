5084ba4a3dbc09ff0d8d91aaa0b32c9be3d178e9
DanielsSon12 <danielouverney67@gmail.com>
2024-01-14 17:29:59 -0300

    projeto_1.5

--- Projetos/Sala do Empreendedor/script.js
+++ Projetos/Sala do Empreendedor/script.js
@@ -3,9 +3,9 @@ function showMenu() {
 
     if (mobileMenu.classList.contains('open')) {
         mobileMenu.classList.remove('open');
-        document.querySelector('.icon').src = "imgs/cardapio.png";
+        document.querySelector('.icon').src = "imgs/menu-aberto.png";
     } else {
         mobileMenu.classList.add('open');
-        document.querySelector('.icon').src = "imgs/letra-x.png";
+        document.querySelector('.icon').src = "imgs/x.png";
     }
 }
